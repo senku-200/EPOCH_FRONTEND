81e8beeeb559205c67f08cc0da5d9c77c28cd3f0
kishoor <kishoor0920@gmail.com>
2024-09-30 16:19:31 +0530

    addtion of the restration in the menu bar

--- mea-app/src/Components/ListComponent/HeaderListContainer.tsx
+++ mea-app/src/Components/ListComponent/HeaderListContainer.tsx
@@ -1,15 +1,23 @@
+"use client";
 import { navigations } from "@/types/NavigationLinks/NavLinks";
 import React from "react";
 import HeaderList from "./HeaderList";
-interface PropType{
-    className?:string;
+import RegisterBtnComponent from "../Common/RegisterBtnComponent";
+import { useMenucontext } from "@/Context/MenuContext/MenuContextProvider";
+interface PropType {
+  className?: string;
 }
-const HeaderListContainer:React.FC<PropType> = ({className=""}) => {
+
+const HeaderListContainer: React.FC<PropType> = ({ className = "" }) => {
+  const { removeVisibility } = useMenucontext();
   return (
     <ul className={`flex gap-3 md:gap-5 transition-all ${className}`}>
       {navigations.map(({ label, url }, index) => (
-        <HeaderList key={index} label={label} url={url} className={className}/>
+        <HeaderList key={index} label={label} url={url} className={className} />
       ))}
+      <li className="block md:hidden" onClick={removeVisibility}>
+        <RegisterBtnComponent styling="text-sm px-3 py-1"/>
+      </li>
     </ul>
   );
 };
